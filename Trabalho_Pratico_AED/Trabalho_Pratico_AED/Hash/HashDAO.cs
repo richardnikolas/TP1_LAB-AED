0b6ba77d5a263c846f9331296a17a62b46a7dae3
Richard Nikolas <richardnikolas1@gmail.com>
2019-11-23 01:55:46 -0300

    Arrumando as bagunças do Phil

--- Trabalho_Pratico_AED/Trabalho_Pratico_AED/Hash/HashDAO.cs
+++ Trabalho_Pratico_AED/Trabalho_Pratico_AED/Hash/HashDAO.cs
@@ -16,82 +16,83 @@ namespace Exercicios.Exercicio1
          * com suporte de:
          *       Fabio Leandro Rodrigues Cordeiro
          */
+
         private LinearHash _hashTable;
         private List<int> outputValues;
         private RichTextBox output_txt;
         static private string path = "C://temp/hash.xml";
 
-        public HashDAO(RichTextBox output_txt){
+        public HashDAO(RichTextBox output_txt) {
             this._hashTable = new LinearHash(1000);
             this.outputValues = new List<int>();
             this.output_txt = output_txt;
             OperationCounter.Increment(3);
         }
-        public void LoadDAO(){
+
+        public void LoadDAO() {
             XmlSerializer ser = new XmlSerializer(typeof(List<int>));
             OperationCounter.Increment();
+
             FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
             OperationCounter.Increment();
+
             try {
                 //Carregar o arquivo xml e jogar na lista:
                 this.outputValues = ser.Deserialize(fs) as List<int>;
                 OperationCounter.Increment();
             }
-            catch(Exception e) {
+            catch (Exception e) {
                 ser.Serialize(fs, this.outputValues);
                 OperationCounter.Increment();
                 throw e;
             }
-            finally{
+            finally {
                 fs.Close();
-                foreach (int elemento in outputValues){
-                    this._hashTable.Insert(elemento);
-                    OperationCounter.Increment();
-                }
-                this._hashTable.SetQuant(outputValues.Count);
                 SaveDao();
                 OperationCounter.Increment(4);
             }
         }
 
-        public List<int> List(){
+        public List<int> List() {
             UpdateOutput();
             return outputValues;
         }
-        public void Insert(int element){
+
+        public void Insert(int element) {
             _hashTable.Insert(element);
             UpdateOutput();
             OperationCounter.Increment(2);
         }
 
-        private void UpdateOutput(){
+        private void UpdateOutput() {
             outputValues = new List<int>();
             OperationCounter.Increment();
             
             foreach (int? n in _hashTable.GetInternalStruct()){
                 OperationCounter.Increment();
-                if(n!=null)
+
+                if (n != null)
                     outputValues.Add((int)n);
             }
         }
 
-        public void Remove(int valueToRemove)
-        {
-            if (_hashTable.GetQuant() > 0){
+        public void Remove(int valueToRemove) {
+            if (_hashTable.GetQuant() > 0) {
               _hashTable.Remove(valueToRemove);
               SaveDao();
               OperationCounter.Increment();
             }
-            else{
+            else
                 this.output_txt.AppendText("Não é possível remover elementos de uma tabela Hash Vazia!\n");
-            }
+            
             OperationCounter.Increment();
         }
-        public void SaveDao(){
+
+        public void SaveDao() {
             this.output_txt.AppendText("Salvando Tabela Hash...\n");
             FileStream fs = null;
             
-            try{
+            try {
                 //Acesso a dados XML (DAO):
                 XmlSerializer ser = new XmlSerializer(typeof(List<int>));
                 OperationCounter.Increment();
@@ -104,7 +105,7 @@ namespace Exercicios.Exercicio1
                 ser.Serialize(fs, this.outputValues);
                 OperationCounter.Increment();
             }
-            catch (Exception e){
+            catch (Exception e) {
                 this.output_txt.AppendText("Ocorreu um erro interno! Exceção: \n" + e.Message+"\n");
             }
             
@@ -113,7 +114,7 @@ namespace Exercicios.Exercicio1
             this.output_txt.AppendText("Tabela Hash Salva!\n");
         }
 
-        public void CleanDao(){
+        public void CleanDao() {
             this.output_txt.AppendText("Limpando Tabela Hash...\n");
             this._hashTable = new LinearHash();
             this.outputValues = new List<int>();
@@ -122,12 +123,12 @@ namespace Exercicios.Exercicio1
             this.output_txt.AppendText("Tabela Hash Limpa!\n");
         }
 
-        public void SortDao(){
+        public void SortDao() {
             this.outputValues.Sort();
             SaveDao();
         }
 
-        public int GetQuant(){
+        public int GetQuant() {
             return _hashTable.GetQuant();
         }
     }
--- Trabalho_Pratico_AED/Trabalho_Pratico_AED/Hash/LinearHash.cs
+++ Trabalho_Pratico_AED/Trabalho_Pratico_AED/Hash/LinearHash.cs
@@ -29,20 +29,24 @@ namespace Exercicios.Exercicio1{
 
         private int GetValidIndex(int element) {
             int validIndex = element % _struct.Length;
-            int aux = element % _struct.Length;//where the loop starts
+            int aux = element % _struct.Length; // where the loop starts
+
+            if (validIndex < 0)
+                validIndex = validIndex * -1;
             
             OperationCounter.Increment(2);
             
-            while (!_struct[validIndex].Equals(null)){
+            while (!_struct[validIndex].Equals(null)) {
                 
                 validIndex++;
                 OperationCounter.Increment();
                 
-                if (validIndex >= _struct.Length){
+                if (validIndex >= _struct.Length) {
                     validIndex = 0;
                     OperationCounter.Increment(2);
                 }
-                if (validIndex.Equals(aux)){
+
+                if (validIndex.Equals(aux)) {
                     OperationCounter.Increment(2);
                     throw new Exception("A estrutura está cheia!");
                 }
@@ -53,53 +57,48 @@ namespace Exercicios.Exercicio1{
 
         public void Insert(int element) {
             int indice = GetValidIndex(element);
-            _struct[indice]= element;
-            ++quant;
+            _struct[indice] = element;
+            quant++;
             OperationCounter.Increment(3);
         }
 
-        public void Remove(int value){
-            if (this.quant > 0){
+        public void Remove(int value) {
+            if (this.quant > 0) {
                 int index = Array.IndexOf(_struct, value);
                 
-                if (index >= 0){    
+                if (index >= 0) {    
                     _struct[Array.IndexOf(_struct, value)] = null;
                     --quant;
                 }
+
                 OperationCounter.Increment(4);
             }
-            else{
-                throw new Exception("Não é possível remover elementos de uma lista vazia!");
-            }
+            else 
+                throw new Exception("Não é possível remover elementos de uma lista vazia!");            
         }
 
-        public int GetQuant(){
-            this.quant = 0;
-            foreach (int?  n in _struct){
-                if (n != null){
-                    this.quant++;
-                    OperationCounter.Increment(2);
-                }
-            }
+        public int GetQuant() {
             return this.quant;
         }
 
-        public void SetQuant(int quant){
+        public void SetQuant(int quant) {
             this.quant = quant;
         }
 
-        public bool IsEmpty(){
+        public bool IsEmpty() {
             OperationCounter.Increment();
+
             if (_struct.Length > 0)
-                return true;
+                return false;
+
             return true;
         }
 
-        public int?[] GetInternalStruct(){
+        public int?[] GetInternalStruct() {
             return this._struct;
         }
 
-        public void SetInternalStruct(int?[] _struct){
+        public void SetInternalStruct(int?[] _struct) {
             this._struct = _struct;
         }
     }
